42ccb951729e304eff0e0f01d3926cf2b75ecad6
22nortpl <minsanglee03@gmail.com>
2026-02-13 09:38:02 +0900

    02-1~3

---
+++ 02/int_convert.py
@@ -0,0 +1,9 @@
+string_a = input("입력a> ")
+int_a = int(string_a)
+
+string_b = input("입력b> ")
+int_b = int(string_b)
+
+#출력합니다.
+print("문자열 자료:", string_a + string_b)
+print("숫자 자료:", int_a + int_b)
---
+++ 02/int_float02.py
@@ -0,0 +1,4 @@
+input_a = float(input("첫 번째 숫자> "))
+input_b = float(input("두 번째 숫자> "))
+
+print("덧셈 결과:", input_a + input_b)
