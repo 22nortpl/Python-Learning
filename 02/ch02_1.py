a74299c042d695e62020af2f1f9ad550d2797e18
22nortpl <minsanglee03@gmail.com>
2026-02-16 00:43:48 +0900

    02-2

---
+++ 02/ch02_1.py
@@ -0,0 +1,4 @@
+radius = int(input("구의 반지름을 입력해주세요: "))
+pi = 3.141592
+print("= 구의 부피는 {:.3f}입니다.".format(radius**3*pi*4/3))
+print("= 구의 부피는 {:.3f}입니다.".format(radius**2*pi*4))
---
+++ 02/ch02_2.py
@@ -0,0 +1,3 @@
+base = int(input("밑변의 길이를 입력해주세요 : "))
+height = int(input("높이의 길이를 입력해주세요 : "))
+print("= 빗변의 길이는 {}cm 입니다.".format(((base**2)+(height**2))**0.5))
